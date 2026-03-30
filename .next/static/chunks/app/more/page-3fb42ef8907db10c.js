7d2e0afb6bb89fb7192578bc888c4bef0461c517
Esther Maduabuchi <prityexsta@gmail.com>
2026-03-30 16:59:23 +0100

    Clean repo for Vercel deployment

--- .next/static/chunks/app/more/page-3fb42ef8907db10c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[12],{74871:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=74871)}),_N_E=n.O()}]);
